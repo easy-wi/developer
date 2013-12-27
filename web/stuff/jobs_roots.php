58e9d378c9927f8f519ad355687eb3a2e6166e2f
Ulrich Block <ulblock@gmx.de>
2013-12-27 16:13:15 +0100

    #17: partial

--- web/stuff/jobs_roots.php
+++ web/stuff/jobs_roots.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * File: jobs_roots.php.
  * Author: Ulrich Block
@@ -37,91 +38,146 @@
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
  */
 
-$rootObject=new rootServer($aeskey);
+$rootObject = new rootServer($aeskey);
+$removeIDs = array('de' => array(), 'vs' => array());
+
+print 'Collecting Dedicated and Vmware server changes'."\r\n";
+
 $query = $sql->prepare("SELECT * FROM `jobs` WHERE (`status` IS NULL OR `status`=1) AND `type` IN ('de','vs')");
 $query2 = $sql->prepare("UPDATE `jobs` SET `status`='3' WHERE `jobID`=? AND `type` IN ('de','vs') LIMIT 1");
 $query3 = $sql->prepare("UPDATE `jobs` SET `status`='2' WHERE `jobID`=? AND `type` IN ('de','vs') LIMIT 1");
 $query4 = $sql->prepare("UPDATE `jobs` SET `status`='1' WHERE `jobID`=? AND `type` IN ('de','vs') LIMIT 1");
 $query->execute();
-print 'Collecting Dedicated and Vmware server changes'."\r\n";
-$removeIDs=array('de' => array(),'vs' => array());
 foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
-    $extraData=@json_decode($row['extraData']);
-    $extraData=(array)$extraData;
-    $type=($row['type'] == 'de') ? 'dedicated' : 'vmware';
-    if (!isset($extraData['runAt']) or strtotime('now')>$extraData['runAt']) {
+
+    $extraData = @json_decode($row['extraData']);
+    $extraData = (array) $extraData;
+
+    $type = ($row['type'] == 'de') ? 'dedicated' : 'vmware';
+
+    if (!isset($extraData['runAt']) or strtotime('now') > $extraData['runAt']) {
+
         $return = $rootObject->rootServer($row['affectedID'], $row['action'],$type,$extraData);
 
         // bei add und mod restart Auftrag schreiben mit extra Data = timestamp
         if ($row['action'] == 'dl') {
+
             if ($return === true) {
+
                 $query2->execute(array($row['jobID']));
-                $command = $gsprache->del." $type server: ".$row['affectedID'].' name:'.$row['name'];
+
+                $command = $gsprache->del . ' ' . $type . ' server: ' . $row['affectedID'] . ' name:' . $row['name'];
+
                 $removeIDs[$row['type']][] = $row['affectedID'];
+
             } else {
+
                 $query4->execute(array($row['jobID']));
-                $command='Error: '.$gsprache->del." $type server: ".$row['affectedID'].' name:'.$row['name'] . ' ' . $return;
+
+                $command = 'Error: ' . $gsprache->del . ' ' . $type . ' server: ' . $row['affectedID'] . ' name:' . $row['name'] . ' ' . $return;
+
             }
+
         } else if ($row['action'] == 'ad') {
+
             $query2->execute(array($row['jobID']));
-            $command = $gsprache->add." $type server: ".$row['affectedID'].' name:'.$row['name'];
-        } else if (in_array($row['action'], array('md','ri','st','rc','rp'))) {
+
+            $command = $gsprache->add . ' ' . $type . ' server: ' . $row['affectedID'] . ' name:' . $row['name'];
+
+        } else if (in_array($row['action'], array('md', 'ri', 'st', 'rc', 'rp'))) {
+
             if ($return === true) {
+
                 $query2->execute(array($row['jobID']));
-                $command = $gsprache->mod." $type server: ".$row['affectedID'].' name: '.$row['name'];
+
+                $command = $gsprache->mod . ' ' . $type . ' server: ' . $row['affectedID'] . ' name: ' . $row['name'];
+
             } else {
+
                 $query4->execute(array($row['jobID']));
-                $command="Error modding $type server: ".$row['affectedID'].' name: '.$row['name'] . ' ' . $return;
+
+                $command = 'Error modding ' . $type . ' server: ' . $row['affectedID'] . ' name: '.$row['name'] . ' ' . $return;
+
             }
+
         } else if ($row['action'] == 're') {
+
             if ($return === true and !isset($extraData->reboot) and strtotime('now')<$extraData->reboot) {
+
                 $query2->execute(array($row['jobID']));
-                $command="Skipped (Re)Start $type server: ".$row['affectedID'].' name: '.$row['name'].' will try later';
+
+                $command = 'Skipped (Re)Start ' . $type . ' server: ' . $row['affectedID'] . ' name: ' . $row['name'] . ' will try later';
+
             } else if ($return === true and (!isset($extraData->reboot) or strtotime('now')>$extraData->reboot)) {
+
                 $query2->execute(array($row['jobID']));
-                $command="(Re)Start $type server: ".$row['affectedID'].' name: '.$row['name'];
+
+                $command = '(Re)Start ' . $type . ' server: ' . $row['affectedID'] . ' name: '.$row['name'];
+
             } else {
+
                 $query4->execute(array($row['jobID']));
-                $command="Error (Re)starting $type server: ".$row['affectedID'].' name: '.$row['name'] . ' ' . $return;
+
+                $command = 'Error (Re)starting ' . $type . ' server: ' . $row['affectedID'] . ' name: ' . $row['name'] . ' ' . $return;
             }
+
         } else if (!isset($customer)) {
-            $command="Error: can not find $type server";
+
+            $command = 'Error: can not find ' . $type . ' server';
+
             $query3->execute(array($row['jobID']));
+
         } else {
-            $command='Error: unknown command';
+            $command = 'Error: unknown command';
         }
+
     } else {
-        $command='Notice: Not the time to run command "Remove PXE boot from DHCP" ('.$row['name'].')';
+        $command = 'Notice: Not the time to run command "Remove PXE boot from DHCP" (' . $row['name'] . ')';
     }
+
     $theOutput->printGraph($command);
+
 }
+
 print "\r\nApplyingPXE changes\r\n";
 $rootObject->PXEFiles();
+
 print "\r\nApplying DHCPchanges\r\n";
+$res = (array) $rootObject->dhcpFiles();
+
 $query = $sql->prepare("INSERT INTO `jobs` (`action`,`date`,`status`,`api`,`type`,`affectedID`,`name`,`hostID`,`userID`,`extraData`,`resellerID`) VALUES ('rp',NOW(),NULL,'J',?,?,?,?,?,?,?)");
 $query2 = $sql->prepare("UPDATE `virtualcontainer` SET `imageid`=? WHERE `id`=? LIMIT 1");
 $query3 = $sql->prepare("UPDATE `rootsDedicated` SET `imageID`=? WHERE `dedicatedID`=? LIMIT 1");
-$res=(array)$rootObject->dhcpFiles();
 foreach ($res as $row) {
     if (isset($row['type'])) {
         $query->execute(array($row['type'], $row['affectedID'], $row['name'], $row['hostID'], $row['userID'],json_encode($row['extraData']), $row['resellerID']));
-        if ($row['type'] == 'de') $query2->execute(array($row['imageID'], $row['affectedID']));
-        else $query3->execute(array($row['imageID'], $row['affectedID']));
+
+        if ($row['type'] == 'de') {
+            $query2->execute(array($row['imageID'], $row['affectedID']));
+        } else {
+            $query3->execute(array($row['imageID'], $row['affectedID']));
+        }
     }
 }
+
 print "\r\n(Re)starting/Stopping dedicated server\r\n";
 $rootObject->startStop();
+
 print "\r\nAdding, Altering, Removing, (Re)starting and Stopping VMWare Virtual Container\r\n";
 $rootObject->VMWare();
+
 print "\r\nRemoving VMWare Virtual Container/Dedicated Server from DB \r\n";
+
 $query = $sql->prepare("DELETE FROM `rootsDedicated` WHERE `dedicatedID`=? LIMIT 1");
 foreach ($removeIDs['de'] as $id) {
-    customColumns('G',$id,'del');
+    customColumns('G', $id, 'del');
     $query->execute(array($id));
 }
+
 $query = $sql->prepare("DELETE FROM `virtualcontainer` WHERE `id`=? LIMIT 1");
 foreach ($removeIDs['vs'] as $id) {
-    customColumns('V',$id,'del');
+    customColumns('V', $id, 'del');
     $query->execute(array($id));
 }
+
 $rootObject = null;
