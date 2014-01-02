1481425096c3130a8ae15303a7b0d076138e1d1c
Ulrich Block <ulblock@gmx.de>
2014-01-02 14:05:03 +0100

    #259: missed one index

--- web/install/install.php
+++ web/install/install.php
@@ -921,7 +921,7 @@ if ($currentStep == 9 and count($systemCheckError) == 0) {
 
     $displayPHPUser = (isset($_SERVER['USER'])) ? $_SERVER['USER'] : 'changeToPHPUser';
 
-    $displayToUser .= "<div class='alert alert-success'>0 */1 * * * ${_SERVER['USER']} cd " . EASYWIDIR . " && timeout 300 php ./reboot.php >/dev/null 2>&1<br>
+    $displayToUser .= "<div class='alert alert-success'>0 */1 * * * ${displayPHPUser} cd " . EASYWIDIR . " && timeout 300 php ./reboot.php >/dev/null 2>&1<br>
 */5 * * * * ${displayPHPUser} cd " . EASYWIDIR . " && timeout 290 php ./statuscheck.php >/dev/null 2>&1<br>
 */1 * * * * ${displayPHPUser} cd " . EASYWIDIR . " && timeout 290 php ./startupdates.php >/dev/null 2>&1<br>
 */5 * * * * ${displayPHPUser} cd " . EASYWIDIR . " && timeout 290 php ./jobs.php >/dev/null 2>&1<br>
