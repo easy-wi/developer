4836dec913bc8209c2a7b19667bcd50e42eb2594
GetYourServer <andreas-beck98@gmx.de>
2015-09-06 22:01:09 +0200

    Update api_external_auth.php

--- web/stuff/admin/api_external_auth.php
+++ web/stuff/admin/api_external_auth.php
@@ -39,7 +39,7 @@
 
 if ($main != 1 or !isset($admin_id) or (isset($admin_id) and !$pa['apiSettings'])) {
     header('Location: admin.php');
-    die('No access);
+    die('No access');
 }
 
 include(EASYWIDIR . '/stuff/keyphrasefile.php');
