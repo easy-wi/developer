ea1629a2d6d4c8cb9fa44900793e702867c16eeb
GetYourServer <andreas-beck98@gmx.de>
2015-09-06 22:03:40 +0200

    Update userpanel_gserver.php

--- web/stuff/user/userpanel_gserver.php
+++ web/stuff/user/userpanel_gserver.php
@@ -37,7 +37,7 @@
  */
 if ((!isset($main) or $main != 1) or (!isset($user_id) or (isset($user_id) and !$pa['restart']))) {
     header('Location: userpanel.php');
-    die('No access);
+    die('No access');
 }
 
 include(EASYWIDIR . '/stuff/keyphrasefile.php');
