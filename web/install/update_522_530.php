73bee5a91977eefdd01f14020611180e117446ed
Ulrich Block <ulblock@gmx.de>
2016-05-01 18:15:49 +0200

    5.30 changelog

--- web/install/update_522_530.php
+++ web/install/update_522_530.php
@@ -43,18 +43,72 @@ if (isset($include) and $include == true) {
     $query = $sql->prepare("INSERT INTO `settings_email_category` (`id`, `name`, `reseller_id`) VALUES (1, 'vServer', 0), (2, 'Server', 0), (3, 'Ticket', 0), (4, 'General', 0), (5, 'VoiceServer', 0), (6, 'GameServer', 0) ON DUPLICATE KEY UPDATE `id`=`id`");
     $query->execute();
 
-    $query = $sql->prepare("INSERT INTO `easywi_version` (`version`,`de`,`en`) VALUES ('5.30','','')");
-    $query->execute();
+    //Install E-Mail template
+    include(EASYWIDIR . '/stuff/data/email_templates.php');
+    foreach ($emailTemplates as $template) {
+        $query = $sql->prepare($template['query']);
+        $query->execute(array(0));
+    }
 
+    $query = $sql->prepare("INSERT INTO `easywi_version` (`version`,`de`,`en`) VALUES
+('5.30','<div align=\"right\">01.05.2016</div>
+<b>&Auml;nderungen:</b><br/>
+<ul>
+<li>Game Server:
+<ul>
+<li>GoTV Demo Upload hinzugefügt</li>
+<li>Trotz falscher RAM Einstellungen können GS hinzugefügt werden</li>
+</ul></li>
+<li>Einstellungen:
+<ul>
+<li>SMTP Connection Test hinzugefügt</li>
+</ul></li>
+<li>Diverses:
+<ul>
+<li>Fehlermeldungen fehlender Tabellen bei DB Repair unterdrückt</li>
+<li>Code Vorbereitung für Semantic Versioning</li>
+<li>HybridAuth auf 2.6.0 aktualisiert</li>
+<li>RSS Feed der Twitter API 1.1 werden unterstützt</li>
+<li>Automatischer Redirect zum install Ordner</li>
+<li>Slogan Support bei der Login Page hinzugefügt</li>
+</ul></li>
+</ul>
+<b>Bugfixes:</b>
+<ul>
+<li>Game Server Umzugsservice benutzt falschen Pfad</li>
+<li>steamclient.so kann bei Valve Servern nicht gefunden werden</li>
+<li>Minecraft Templates</li>
+</ul>','<div align=\"right\">01.05.2016</div>
+<b>Changes:</b><br/>
+<ul>
+<li>Game server:
+<ul>
+<li>Added GoTV demo upload support</li>
+<li>Ensure that incorrect RAM settings do not prevent gs add</li>
+</ul></li>
+<li>Settings:
+<ul>
+<li>Added SMTP connection</li>
+</ul></li>
+<li>Miscellaneous:
+<ul>
+<li>Supressed error return of missing tables at DB repair</li>
+<li>Code preperation for semantic versioning</li>
+<li>Upgraded HybridAuth 2.6.0</li>
+<li>Added RSS Feed with Twitter API 1.1</li>
+<li>Automatic redirection to install folder</li>
+<li>Slogan support at login page</li>
+</ul></li>
+</ul>
+<b>Bugfixes:</b>
+<ul>
+<li>Migrate game server using incorrect path</li>
+<li>steamclient.so not found on valve server start up</li>
+<li>Minecraft templates</li>
+</ul>')");
+    $query->execute();
     $response->add('Action: insert_easywi_version done: ');
     $query->closecursor();
-    
-     //Install E-Mail template
-     include(EASYWIDIR . '/stuff/data/email_templates.php');
-     foreach ($emailTemplates as $template) {
-         $query = $sql->prepare($template['query']);
-         $query->execute(array(0));
-     }
 
 } else {
     echo "Error: this file needs to be included by the updater!<br />";
